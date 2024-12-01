cd495a8f977bf52c103cfd6563ad3ead426866f1
Namgyu11 <103015031+Namgyu11@users.noreply.github.com>
2024-12-01 22:15:58 +0900

    Initial commit

---
+++ src/test/java/com/springcloud/client/msapractice/MsaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.client.msapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
